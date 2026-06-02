1385bd053bd5e993e74ba46fa34e677d27036494
Guy Shabat <g.y.shabat@gmail.com>
2026-06-02 17:44:27 +0300

    Complete callbacks ex1

---
+++ callbacks/ex1/index.js
@@ -0,0 +1,14 @@
+const push = function () {
+  console.log("pushing it!")
+}
+
+const pull = function () {
+  console.log("pulling it!")
+}
+
+const pushPull = function(func) {
+  func();
+};
+
+pushPull(push)
+pushPull(pull)
