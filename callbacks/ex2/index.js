631d22a988acc6b5bb8439edae0dd82d1f011fb2
Guy Shabat <g.y.shabat@gmail.com>
2026-06-02 17:54:19 +0300

    Complete callbacks ex2

---
+++ callbacks/ex2/index.js
@@ -0,0 +1,10 @@
+const returnTime = function (time) {
+  console.log('The current time is: ' + time)
+}
+
+const getTime = function(func) {
+    const time = new Date();
+    func(time);
+}
+
+getTime(returnTime)
