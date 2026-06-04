8f6672341f634c2f1b28b7679194f5184b73b05a
Guy Shabat <g.y.shabat@gmail.com>
2026-06-04 15:39:34 +0300

    Add exercises -  this ex6.1

--- this/ex6/index.js
+++ this/ex6/index.js
@@ -1,30 +1,57 @@
 const coffeeShop = {
   beans: 40,
+  money: 100,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte: { beanRequirement: 10, price: 5 },
+    americano: { beanRequirement: 5, price: 3 },
+    doubleShot: { beanRequirement: 15, price: 8 },
+    frenchPress: { beanRequirement: 12, price: 6 }
   },
 
   makeDrink: function (drinkType) {
     if (!this.drinkRequirements[drinkType]) {
       console.log("Sorry, we don't make " + drinkType);
-      return;
+      return false; 
     }
 
-    if (this.beans < this.drinkRequirements[drinkType]) {
+    if (this.beans < this.drinkRequirements[drinkType].beanRequirement) {
       console.log("Sorry, we're all out of beans!");
-      return;
+      return false;
     }
 
-    this.beans -= this.drinkRequirements[drinkType];
+    this.beans -= this.drinkRequirements[drinkType].beanRequirement;
+    return true;
+  },
+
+  buyBeans: function (numBeans) {
+    const costPerBean = 0.5;
+    const totalCost = numBeans * costPerBean;
+
+    if (this.money >= totalCost) {
+      this.money -= totalCost;
+      this.beans += numBeans;
+      console.log(`Bought ${numBeans} beans. Remaining money: $${this.money}`);
+    } else {
+      console.log("Not enough money to buy beans!");
+    }
+  },
+  buyDrink: function (drinkType) {
+    const drinkMade = this.makeDrink(drinkType);
+    
+    if (drinkMade) {
+      this.money += this.drinkRequirements[drinkType].price;
+      console.log(`Sold a ${drinkType}. Total money: $${this.money}`);
+    }
   }
 };
+
+
 
 coffeeShop.makeDrink("latte"); 
 coffeeShop.makeDrink("americano");
 coffeeShop.makeDrink("filtered"); 
 coffeeShop.makeDrink("doubleShot");
 coffeeShop.makeDrink("frenchPress");
+coffeeShop.buyBeans(20); 
+coffeeShop.buyDrink("frenchPress");
