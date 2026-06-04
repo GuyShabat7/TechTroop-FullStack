7f0560a6147e400a1970d105dfb5c196cc8a57ac
Guy Shabat <g.y.shabat@gmail.com>
2026-06-04 18:59:15 +0300

    Add exercises - closures ex3

---
+++ closures/ex3/index.js
@@ -0,0 +1,23 @@
+const SongsManager = function () {
+    const songs = {};
+    const youtubeBaseUrl = "https://www.youtube.com/watch?v=";
+
+    const addSong = (name, url) => {
+        const videoId = url.split('v=')[1];
+        songs[name] = videoId;
+    }
+    const fullUrl = name => youtubeBaseUrl + songs[name];   
+    const getSong = name => console.log(fullUrl(name));
+
+    return {
+        addSong: addSong,
+        getSong: getSong
+    };
+}
+
+const songsManager = SongsManager()
+songsManager.addSong("sax", "https://www.youtube.com/watch?v=3JZ4pnNtyxQ")
+songsManager.addSong("how long", "https://www.youtube.com/watch?v=CwfoyVa980U")
+songsManager.addSong("ain't me", "https://www.youtube.com/watch?v=D5drYkLiLI8")
+
+songsManager.getSong("sax") // should print https://www.youtube.com/watch?v=3JZ4pnNtyxQ
