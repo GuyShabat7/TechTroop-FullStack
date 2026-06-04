8a766e6420101a8da87e3d979c50d4d2bde44f64
Guy Shabat <g.y.shabat@gmail.com>
2026-06-04 15:33:56 +0300

    Add exercises -  this ex6

---
+++ this/ex6/index.js
@@ -0,0 +1,30 @@
+const coffeeShop = {
+  beans: 40,
+
+  drinkRequirements: {
+    latte: 10,
+    americano: 5,
+    doubleShot: 15,
+    frenchPress: 12
+  },
+
+  makeDrink: function (drinkType) {
+    if (!this.drinkRequirements[drinkType]) {
+      console.log("Sorry, we don't make " + drinkType);
+      return;
+    }
+
+    if (this.beans < this.drinkRequirements[drinkType]) {
+      console.log("Sorry, we're all out of beans!");
+      return;
+    }
+
+    this.beans -= this.drinkRequirements[drinkType];
+  }
+};
+
+coffeeShop.makeDrink("latte"); 
+coffeeShop.makeDrink("americano");
+coffeeShop.makeDrink("filtered"); 
+coffeeShop.makeDrink("doubleShot");
+coffeeShop.makeDrink("frenchPress");
